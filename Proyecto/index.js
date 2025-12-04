bfe5c22c2b4fa1f7f01b45aa0cfdfcdde78911a4
Juan Aristizabal <jearistizabalv@ut.edu.co>
2025-12-04 11:02:35 -0500

    feat:Avance del proyecto CSS terminado, avance del JS

--- Proyecto/index.js
+++ Proyecto/index.js
@@ -32,23 +32,118 @@ let totalPaginas = 1;
 
 /* Creacion de funciones principales*/
  
+/* Uso del local storage para el modo oscuro */
 function inicializarTema(){
+    const modoOscuroGuardado = localStorage.getItem("modoOscuro");
+
+    if (modoOscuroGuardado === "true") {
+        document.body.classList.add("dark-mode");
+        toggleModoOscuro.checked = true;
+    }
 
 }
 function modoOscuro(){
+    document.body.classList.toggle("dark-mode");
+
+    if (document.body.classList.contains("dark-mode")) {
+        localStorage.setItem("modoOscuro", "true");
+    } else {
+        localStorage.setItem("modoOscuro", "false");
+    }
 
 }
-async function buscarPersonajes(){
 
-}
-function mostrarPersoanjes(){
+/* Uso de la API */
+async function buscarPersonajes(urlAPI){
+    try {
+        textoEstado.textContent = "Cargando...";
+        textoEstado.computedStyleMap.display = "block";
 
+        const respuesta = await fetch(urlAPI);
+
+        if (!respuesta.ok) {
+            throw new Error(`Error en la solicitud" ${respuesta.status}`);
+        }
+    const datosJSON = await respuesta.json();
+
+    totalPaginas = datosJSON.info.pages;
+
+    actualizarBotonesPagina();
+
+    mostrarPersoanjes(datosJSON.results);
+
+    textoEstado.textContent = "Listo";
+} catch (error) {
+    console.error("Error al obtener los personajes:", error);
+    textoEstado.textContent = "Error al cargar los personajes.";
+    textoEstado.style.display = "block";
 }
+}
+
+ /* USO DEL DOM PARA MOSTRAR LOS RESULTADOS */
+/**
+ * @param {Array} personajes
+ */
+function mostrarPersoanjes(personajes){
+    contenedorResultados.innerHTML = "";
+
+    if (personajes.length === 0) {
+        contenedorResultados.innerHTML = "<p>No se encontraron personajes.</p>";
+        return;
+    }
+    personajes.forEach(function(personaje){
+        const tarjetaPersonaje = document.createElement("div");
+        tarjetaPersonaje.classList.add("character-card");
+        tarjetaPersonaje.setAttribute("role", "article");
+
+        const colorEstado = colorEstado(personaje.status);
+
+        /* Contenido de la tarjeta */
+        tarjetaPersonaje.innerHTML = `
+        <img 
+        src="${personaje.image}" 
+        alt="Imagen de ${personaje.name}"
+        loading="lazy"
+      >
+      <div class="card-content">
+        <h3>${personaje.name}</h3>
+        <p>
+          <strong>Estado:</strong> 
+          <span style="color: ${colorEstado}; font-weight: bold;">
+            ${personaje.status}
+          </span>
+        </p>
+        <p><strong>Especie:</strong> ${personaje.species}</p>
+        <p><strong>Género:</strong> ${personaje.gender}</p>
+        <p><strong>Origen:</strong> ${personaje.origin.name}</p>
+        <p><strong>Ubicación:</strong> ${personaje.location.name}</p>
+      </div>
+    `;
+
+        contenedorResultados.appendChild(tarjetaPersonaje);
+    });
+}
+
+/* Actualiza el estado de los botones segun la pagina */
+
 function actualizarBotonesPagina(){
-
+    botonAnterior.disabled = numeroPaginaActual === 1;
+    botonSiguiente.disabled = numeroPaginaActual === totalPaginas;
+    numeroPagina.textContent = `Página ${numeroPaginaActual} de ${totalPaginas}`;
  }
- function colorEstado(estado){
 
+/**
+ * @param {string} estado
+ * @return {string}
+ */
+function colorEstado(estado){
+    const colores = {
+        "Alive": "green",
+        "Dead": "red",
+        "unknown": "orange"
+    };
+
+    return colores[estado] || "grey";
  }
 
  function urlAPI (){
