e0ca42c4ad441e0021eca20df1ae978335b02da9
Juan Aristizabal <jearistizabalv@ut.edu.co>
2025-12-03 21:50:41 -0500

    feat: Creacion de las funciones del poryecto y normalizacion de los estilos

--- Proyecto/index.js
+++ Proyecto/index.js
@@ -4,3 +4,59 @@
 
 const urlBaseAPI = "https://rickandmortyapi.com/api/character";
 
+/* Selectores de busqueda */
+
+const contenedorResultados = document.querySelector("results");
+const inputBusqueda = document.querySelector("#searchInput");
+const botonBusqueda = document.querySelector("#searchBtn");
+const cargarPrimeros = document.querySelector("#loadBtn");
+const textoEstado = document.querySelector("#status");
+
+
+/* Selectores de paginacion */
+
+const botonAnterior = document.querySelector("#prevBtn");
+const botonSiguiente = document.querySelector("#nextBtn");
+const numeroPagina = document.querySelector("#pageInfo");
+const busqueda = document.querySelector("#searchForm");
+
+/* Selector de modo oscuro */
+
+const toggleModoOscuro = document.querySelector("#darkModeBtn");
+
+/* Variables para la paginacion */
+
+let numeroPaginaActual = 1;
+let totalPaginas = 1;
+
+
+/* Creacion de funciones principales*/
+ 
+function inicializarTema(){
+
+}
+function modoOscuro(){
+
+}
+async function buscarPersonajes(){
+
+}
+function mostrarPersoanjes(){
+    
+}
+function actualizarBotonesPagina(){
+
+ }
+ function colorEstado(estado){
+
+ }
+
+ function urlAPI (){
+
+ }
+ function limpiarBusqueda(){
+
+ }
+function inicializar(){
+
+}
